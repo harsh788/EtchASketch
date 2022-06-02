fafad59a43d9698fd7e71fb49cc347ce288309c2
harsh788 <Harsh.Shah@iiitb.org>
2022-06-02 23:11:25 +0530

    Random colouring done

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// for(let i=0;i<16;i++)
-// {
-//     var newDiv = document.createElement('div');
-//     newDiv.className = 'ydirection';
-//     newDiv.style.border = 'solid #000';
-//     newDiv.style.borderWidth = '2px';
-//     newDiv.style.display = 'flex';
-//     newDiv.style.backgroundColor = 'Yellow';
-//     newDiv.textContent = 'Hello World';
-//     document.getElementsByClassName('container')[0].appendChild(newDiv);
-//     newDiv.style.width = parent.innerWidth; 
-// }
-
-// let container = document.getElementsByClassName('container');
-// console.log(container[0].scrollWidth);
-// for(let i=0;i<10;i++)
-// {
-//     let row = document.createElement('tr');
-//     row.style.flex = '1 1 0%';
-//     row.id = 'row' + i;
-//     row.className = 'rows';
-
-//     container[0].appendChild(row);
-
-//     for(let j=0;j<10;j++)
-//     {
-//         let cell = document.createElement('td');
-//         cell.style.flex = '1 1 0%';
-//         cell.style.border = 'solid #000';
-//         cell.style.borderWidth = '2px';
-//         //cell.innerHTML = `${i}${j}`;
-//         row.appendChild(cell);
-//     }
-
-// }
 const container = document.getElementById('container');
 defaultGrid(16);
 
@@ -45,3 +10,14 @@ function defaultGrid(numOfRows){
     }
 }
 
+let nodeListCell = document.querySelectorAll("#cell");
+let arrayCell = Array.from(nodeListCell);
+
+arrayCell.forEach((div, index) => div.addEventListener("mouseover", (e) => { arrayCell[index].style.backgroundColor = RGB(); }));
+
+function RGB(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
